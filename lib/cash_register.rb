15026f013a47ea029bcb47bc4707103478674bb0
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-03 17:40:30 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,5 +7,7 @@ class CashRegister
   end
   def add_item(title,price,quantity = 1) #defaulted to 1
     @total += (price * quantity)
+    #expected `cash_register.total` to have changed by 15.0, but was changed by 5.0
+    #on failure.
   end
 end
