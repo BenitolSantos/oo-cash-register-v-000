dfed9f6a32340251332a9c84d7a44f53a5bc962b
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-03 19:25:21 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,6 +13,7 @@ class CashRegister
   def apply_discount
     @total = @total - (@total * (@discount/100))
     #discount is a percent because it expects 200 off not 20
+    puts "After the discount, the total comes to $800"
     @total
   end
 end
