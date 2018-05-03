c730167c7d2b7708f6589987273489cc19f1ea2a
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-03 17:11:14 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :discount 
+  attr_accessor :discount #discount is a attribute
   def initialize(discount = 0) #argument with a default value of 0
     @total = 0
     @discount = discount
