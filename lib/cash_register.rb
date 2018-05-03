0333c407086e8855961b7aef00911bb9f3da4ba0
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-03 20:02:59 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,15 @@
 class CashRegister
-  @@items = []
-  attr_accessor :discount,:total #discount is a attribute, total is an atrribute too
+  
+  attr_accessor :discount,:total, :items #discount is a attribute, total is an atrribute too
   #because it returns a total
   def initialize(discount = 0) #argument with a default value of 0
     @total = 0
+    @items = [] #items should be an
     @discount = discount #everything instance variable becomes an attribute
   end
   def add_item(title,price,quantity = 1) #defaulted to 1
     @title = title
-    @@items << @title
+    @items << @title
     @total += (price * quantity)
     #expected `cash_register.total` to have changed by 15.0, but was changed by 5.0
     #on failure. so I changed it
