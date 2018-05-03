17a2763372a98ef8bd3d35a261c0afcd7e8d79b7
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-03 20:07:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   end
   def add_item(title,price,quantity = 1) #defaulted to 1
     @title = title
-    quantity.times {@items << @title} #add as many items as needed. 
+    quantity.times {@items << @title} #add as many items as needed.
     #the quantity of each to the array
     @total += (price * quantity)
     #expected `cash_register.total` to have changed by 15.0, but was changed by 5.0
